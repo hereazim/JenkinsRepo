526f4930520210da973d2fd40fe646a3c106f3aa
hereazim <106958262+hereazim@users.noreply.github.com>
2023-03-02 10:31:24 +0530

    Line Removed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("This is HelloWorld java program");
 		System.out.println("Line 1 included..");
-		System.out.println("Line 2 included..");
 	}
 }
