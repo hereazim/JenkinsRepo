5f9ef78322501a0301f3c36b1248417747e9c142
hereazim <106958262+hereazim@users.noreply.github.com>
2023-03-02 12:20:38 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,11 @@
+public class OddEven{
+ public static void main(String[] args){
+   int n=78;
+   if(n%2==0){
+    System.out.println("The given number is even"); 
+   }
+   else{
+    System.out.println("The given number is odd"); 
+   }
+ }
+}
