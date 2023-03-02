dbddacbccbcdcfa504d530ec017cfc21d9c9a398
hereazim <106958262+hereazim@users.noreply.github.com>
2023-03-02 13:06:29 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -1,7 +1,6 @@
 public class OddEven{
  public static void main(String[] args){
-   int n=178;
-   if(n%2==0){
+   if(73473%2==0){
     System.out.println("The given number is even"); 
    }
    else{
